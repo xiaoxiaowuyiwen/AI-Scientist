c54379df76b80bf8f72938708a4f4a7823f15e7f
wushaoyun <1073710317@163.com>
2024-09-17 11:13:47 +0800

    调整generate_ideas的逻辑，主要是prompt的逻辑

--- ai_scientist/generate_ideas.py
+++ ai_scientist/generate_ideas.py
@@ -4,17 +4,56 @@ import os.path as osp
 import time
 import traceback
 from typing import List, Dict, Union
-from ai_scientist.llm import get_response_from_llm, extract_json_between_markers
 
-import requests
 import backoff
+import requests
+
+from ai_scientist.llm import get_response_from_llm, extract_json_between_markers
 
 S2_API_KEY = os.getenv("S2_API_KEY")
 
-idea_first_prompt = """{task_description}
+idea_first_prompt_bak = """{task_description}
 <experiment.py>
 {code}
 </experiment.py>
+
+Here are the ideas that you have already generated:
+
+'''
+{prev_ideas_string}
+'''
+
+Come up with the next impactful and creative idea for research experiments and directions you can feasibly investigate with the code provided.
+Note that you will not have access to any additional resources or datasets.
+Make sure any idea is not overfit the specific training dataset or model, and has wider significance.
+
+Respond in the following format:
+
+THOUGHT:
+<THOUGHT>
+
+NEW IDEA JSON:
+```json
+<JSON>
+```
+
+In <THOUGHT>, first briefly discuss your intuitions and motivations for the idea. Detail your high-level plan, necessary design choices and ideal outcomes of the experiments. Justify how the idea is different from the existing ones.
+
+In <JSON>, provide the new idea in JSON format with the following fields:
+- "Name": A shortened descriptor of the idea. Lowercase, no spaces, underscores allowed.
+- "Title": A title for the idea, will be used for the report writing.
+- "Experiment": An outline of the implementation. E.g. which functions need to be added or modified, how results will be obtained, ...
+- "Interestingness": A rating from 1 to 10 (lowest to highest).
+- "Feasibility": A rating from 1 to 10 (lowest to highest).
+- "Novelty": A rating from 1 to 10 (lowest to highest).
+
+Be cautious and realistic on your ratings.
+This JSON will be automatically parsed, so ensure the format is precise.
+You will have {num_reflections} rounds to iterate on the idea, but do not need to use them all.
+"""
+
+# 相比原来的idea_first_prompt，去掉了experiment.py的内容
+idea_first_prompt = """{task_description}
 
 Here are the ideas that you have already generated:
 
@@ -129,7 +168,8 @@ def generate_ideas(
             print(f"Iteration 1/{num_reflections}")
             text, msg_history = get_response_from_llm(
                 idea_first_prompt.format(
-                    task_description=prompt["task_description"],
+                    # task_description=prompt["task_description"],
+                    task_description=idea_system_prompt,  # 原始的description与llm强相关，此处先直接用system message代替
                     code=code,
                     prev_ideas_string=prev_ideas_string,
                     num_reflections=num_reflections,
@@ -225,7 +265,8 @@ def generate_next_idea(
                 print(f"Iteration 1/{num_reflections}")
                 text, msg_history = get_response_from_llm(
                     idea_first_prompt.format(
-                        task_description=prompt["task_description"],
+                        # task_description=prompt["task_description"],
+                        task_description=idea_system_prompt,  # 原始的description与llm强相关，此处先直接用system message代替
                         code=code,
                         prev_ideas_string=prev_ideas_string,
                         num_reflections=num_reflections,
